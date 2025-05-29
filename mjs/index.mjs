0632e496ba8cbba730156cd77e76fd17468ce405
dalpo <diegopasten78@gmail.com>
2025-05-28 23:24:37 -0400

    feat: :fire: Ejemplo de ES Module

---
+++ mjs/index.mjs
@@ -0,0 +1,12 @@
+// .js -> por defecto utiliza CommonJS
+// .mjs -> para utilizar ES Module
+// .cjs -> para utilizar CommionJS
+
+/* C03: ES Module */
+import {sum, sub, mult} from './sum.mjs'
+
+console.log(sum(4,2));
+console.log(sub(4,2));
+console.log(mult(4,2));
+
+
