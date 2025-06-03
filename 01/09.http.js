9cf1fb457791e645ab6e2030f333085ca700e168
Diego Pasten <diegopasten78@gmail.com>
2025-06-02 23:48:23 -0400

    feat: :bulb: Server de Node

--- 01/09.http.js
+++ 01/09.http.js
@@ -1,9 +1,25 @@
-const http = require('node:http')
+/* C15 Servidor de Node */
+const http = require('node:http') /*<-- Importar el módulo */
+
+//Importación e integración de modulo de fichero 10
+const {findAvailablePort} = require('./10.free-port.js')
+
+// Creación del servidor:
 const server = http.createServer((req, res) => {
     console.log("Request received...")
     res.end("Hola Mundo")
 })
 
-server.listen(0, () => {
-    console.log(`Server listening on port http://localhost:${server.address().port}`)
+//Buscar si funciona un puerto en especificio, sino busca otro
+findAvailablePort(2134).then(port => {
+    server.listen(port, () => {
+        console.log(`server  listenting on port http://localhost:${port}`)
+    })
 })
+
+
+
+//Indicar al sistema que utilice un puerto disponible "al azar"
+/* server.listen(0, () => {
+    console.log(`Server listening on port http://localhost:${server.address().port}`)
+}) */
